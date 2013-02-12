63cbcb903a8b9418a7e6985260bb2c0fdf27ed53
John Weis <weis.john@gmail.com>
2013-02-11 22:07:18 -0500

    randal now accepts stdin redirection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+
 // Randal helps pick the order for the game
 var randal = module.exports = function() {
     var args = Array.prototype.slice.call(arguments, 0);
@@ -11,6 +13,20 @@ var randal = module.exports = function() {
     return shuffle;
 }
 
+// support data from stdin
+var data = "";
+process.stdin.on('data', function() {
+    data += chunk;
+});
+process.stdin.on('write', function() {
+    data = data.split(' ');
+    if (data.length) {
+        console.log('data');
+        console.log(randal.apply(null, data).join(' '));    
+    }
+});
+
+
 var program = require('commander');
 program
     .version('0.0.3')
@@ -21,4 +37,13 @@ program
     })
     .parse(process.argv);
 
-console.log(randal.apply(null, program.args).join(' '));
+// don't output an empty list
+if (program.args.length) {
+    var args = program.args;
+    if (program.args.length === 1 && program.args[0] == "/dev/fd/63") {    
+        args = fs.readFileSync("/dev/fd/63");
+        args = args.toString('utf-8').replace("\n", "").split(" ");
+    } 
+
+    console.log(randal.apply(null, args).join(' '));    
+}
