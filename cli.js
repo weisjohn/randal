1bab5f3d4c2f0b4c6f2a8654bbfaaa83eacca958
Kevin Jones-Evans <kevin.jones-evans@tangramflex.com>
2021-03-15 14:16:45 -0400

    Adds play-sound instead of play, shortens sound file

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs')
   , randal = require('./index')
-  , play = require('play')
+  , player = require('play-sound')(opts = {})
   ;
 
 // set up the cli
@@ -14,13 +14,17 @@ program
         console.log('    $ randal joe sally bob');
         console.log('    bob joe sally\n');
     })
-    .option('-d, --drumroll', 'Play a drumroll for a couple seconds before displaying output')
+    .option('-d, --drumroll', 'Play a drumroll before displaying output')
     .parse(process.argv);
 
 function ret(args) {
     if (program.drumroll) {
-        play.sound('drumroll.wav', function() {
+        player.play('drumroll.wav', function(err) {
             console.log(randal.apply(null, args).join(" "));
+            if (err) {
+                console.log('Error occurred. Please install a player listed here: https://github.com/shime/play-sound');
+                throw err
+            } 
         });
     } else {
         console.log(randal.apply(null, args).join(" "));
